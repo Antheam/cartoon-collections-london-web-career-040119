b0a2bb06a2330ccfce40c6ae4442206c41be7a39
Anthea Merton <anthea.merton@gmail.com>
2019-03-22 19:49:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
-  planeteer_calls.collect { |call| call.capitalize + '!' }
+  planeteer_calls.collect { |call| call.upcase + '!' }
 end
 
 def long_planeteer_calls(planeteer_calls)# code an argument here
